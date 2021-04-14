adef0d7f95c94c019087b55fa17e6c50c7415bd2
Caroline Vieira <carolinesanvieira@gmail.com>
2021-04-14 14:30:24 -0300

    Working with Content

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,15 +1,37 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 
 const ComponentsScreen = () => {
-    return <Text style={styles.textStyles}>This is the component screen</Text>
+    const name = 'Carol';
+    return (
+        <View>
+            <Text style={styles.textStyles}>Getting started with React Native</Text>
+            <Text style={styles.textSubHeader}>My name is {name}</Text>   
+        </View>
+    
+
+    ); 
 };
 
 
 const styles = StyleSheet.create({
     textStyles: {
-        fontSize: 30
-    }
+        
+        fontSize: 45,
+        fontWeight: '600',
+        alignSelf: 'center',
+        marginVertical: 40,
+    
+    },
+
+    textSubHeader: {
+        color: '#7F00FF',
+        fontSize: 20,
+        fontWeight: '600',
+        
+    
+   }
+    
 
 });
 
