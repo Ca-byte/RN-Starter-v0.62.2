943c23abc1b3895956d019affb9e3dfea4523c91
Caroline Vieira <carolinesanvieira@gmail.com>
2021-04-20 16:24:14 -0300

    Itroduction to Reducers

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,21 +1,37 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { View, Text, StyleSheet, Button} from 'react-native';
 
+const reducer = ( state, action ) => {
+    // state=== { count: number}
+    // action === { type: 'increase' || 'decrease', payload: '}
+    switch(action.type){
+        case 'increment':
+            return {...state, count: state.count + action.payload};
+        case 'decrement':
+            return {...state, count: state.count - action.payload};
+        default:
+            return state;
+    }
+
+};
 
 const CounterScreen = () => {
-    const [ Counter, setCounter ] = useState(0);
+    const [state, dispatch] = useReducer(reducer, { count: 0})
+ 
     return (
         <View>
 
             <Button title='Increase' onPress={()=> {
-                setCounter(Counter + 1);
+                dispatch({ type: 'increment', payload:1 })
+               
             }}
             />
             <Button title='Decrease' onPress={() => {
-                setCounter(Counter - 1);
+                dispatch({ type: 'decrement', payload:1 })
+               
             }}
             />
-            <Text>Current Counter:{Counter}</Text>
+            <Text>Current Counter:{state.count}</Text>
         </View>
 
     );
